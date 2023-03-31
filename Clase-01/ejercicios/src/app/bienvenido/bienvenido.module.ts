f2f65b030fb350f3e7f16cf4905564fdf16cdf5d
Lorenzo Cea <lcea@HEXACTA.COM>
2023-03-30 23:31:54 -0300

    base rutas ej 2

---
+++ Clase-01/ejercicios/src/app/bienvenido/bienvenido.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BienvenidoComponent } from './bienvenido.component';
+
+
+
+@NgModule({
+  declarations: [
+    BienvenidoComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class BienvenidoModule { }
